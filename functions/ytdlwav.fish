17ff5849b25cfbbd13fcbc9cff4204db3d5afb9f
aisair <ais11air@gmail.com>
2023-02-12 18:05:04 -0600

    updated the ytdlwav.fish function to use temporary files

--- functions/ytdlwav.fish
+++ functions/ytdlwav.fish
@@ -13,45 +13,36 @@ function ytdlwav --description "Downloads file using yt-dlp then converts to AAC
         echo 'you forgor the link 💀'
         return 1
     end
+
     argparse "h/help" "k/keep" -- $argv
     or begin
         set_color yellow
-	echo "warn: argument parsing failed!"
-	set_color normal
+        echo "warn: argument parsing failed!"
+        set_color normal
     end
     
     if set -q _flag_h
-        echo "ytdlwav Help"
-	echo "Downloads file using yt-dlp then converts to WAV using ffmpeg"
-	echo "Usage:"
-	echo "ytdlwav [-hk] target_url"
-	echo -e \n"-h or --help: prints this help sheet"
-	echo "-k or --keep: keeps all intermediary files"
-	return
+        builtin printf '%s\n' "ytdlwav Help" "Downloads file using yt-dlp then converts to WAV using ffmpeg" "Usage:" "ytdlwav [-hk] target_url" "-h or --help: prints this help sheet" "-k or --keep: keeps all intermediary files"
+        return
     end
-
-    set SONGTITLE (yt-dlp --print '%(title)s' $argv[1])
-    set SONGUUID (uuidgen)
-
-    echo "downloading $SONGTITLE"
-    set ytFILE (yt-dlp -o "$SONGUUID.%(ext)s" --exec echo -q -f ba -x $argv[1])
-    echo "downloaded $SONGTITLE to $ytFILE"
     
-    echo "converting $(basename $ytFILE) to WAV"
-    ffmpeg -loglevel error -i $ytFILE $SONGUUID.wav
-    echo "converted $(basename $ytFILE) to $SONGUUID.wav"
+    set title (yt-dlp --print '%(title)s' $argv[1])
+    set tmpdir (mktemp -d -t $title[1])
 
-    echo "converting $SONGUUID.wav to AAC"
+    builtin echo "downloading $title"
+    set ytfile (yt-dlp -P $tmpdir --exec "builtin echo" -q -f ba -x --audio-format wav $argv[1])
+
+    builtin echo "converting $title to AAC"
     # see apple-digital-masters.pdf (soundcheck was too quiet)
-    afconvert $SONGUUID.wav $SONGUUID.caf -d 0 -f caff
-    afconvert $SONGUUID.caf -d aac -f m4af -u pgcm 2 -b 256000 -q 127 -s 2 $SONGTITLE.m4a
-    echo "converted $SONGUUID.wav to $SONGTITLE.m4a"
+    set filename (string split -m1 -r -f1 "." $ytfile)
+    afconvert $ytfile $filename.caf -d 0 -f caff
+    afconvert $filename.caf -d aac -f m4af -u pgcm 2 -b 256000 -q 127 -s 2 $title.m4a
     
     if not set -q _flag_k
-    	$TRASH_EXECUTABLE $ytFILE $SONGUUID.wav $SONGUUID.caf
-    	echo "deleted intermediary files $(basename $ytFILE), $SONGUUID.wav, and $SONGUUID.caf"
+    	$TRASH_EXECUTABLE $tmpdir
+    	builtin echo "deleted intermediary files ($(basename $ytfile) and $(basename $filename.caf))"
     end
 
-    eval $PICARD_EXECUTABLE \"$PWD/$SONGTITLE.m4a\"
+    eval $PICARD_EXECUTABLE \"$PWD/$title.m4a\"
 end
 
