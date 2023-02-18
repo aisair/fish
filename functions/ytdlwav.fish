7a192f5fbcbaf807e6fc35438f551f941a4a26cf
aisair <ais11air@gmail.com>
2023-02-18 16:08:31 -0600

    added playlist functionality to ytdlwav (still keeping ytplwav but it is redundant)

--- functions/ytdlwav.fish
+++ functions/ytdlwav.fish
@@ -1,48 +1,70 @@
-#!/usr/bin/env fish
-
-function ytdlwav --description "Downloads file using yt-dlp then converts to AAC using ffmpeg and afconvert"
+function ytdlwav --description "Downloads file or playlist using yt-dlp then converts to AAC using ffmpeg and afconvert"
     if type -q trash
         set TRASH_EXECUTABLE trash
     else
         set TRASH_EXECUTABLE rm
     end
+    
     # set PICARD_EXECUTABLE "/Applications/MusicBrainz Picard.app/Contents/MacOS/picard-run"
     set PICARD_EXECUTABLE "open -a MusicBrainz\ Picard"
 
+    argparse "d/disable-picard" "h/help" "k/keep" -- $argv
+    or begin
+        set_color yellow
+        builtin echo "warn: argument parsing failed! ⚠️"
+        set_color normal
+        return 1
+    end
+    
     if not count $argv > /dev/null
-        echo 'you forgor the link 💀'
+        builtin echo 'you forgor the link 💀'
         return 1
     end
 
-    argparse "h/help" "k/keep" -- $argv
-    or begin
-        set_color yellow
-        echo "warn: argument parsing failed!"
-        set_color normal
-    end
-    
     if set -q _flag_h
-        builtin printf '%s\n' "ytdlwav Help" "Downloads file using yt-dlp then converts to WAV using ffmpeg" "Usage:" "ytdlwav [-hk] target_url" "-h or --help: prints this help sheet" "-k or --keep: keeps all intermediary files"
+        builtin printf '%s\n' "ytdlwav Help" "Downloads file or playlist using yt-dlp then converts to AAC using ffmpeg and afconvert" "Usage:" "ytdlwav [-hk] target_url" "-d or --disable-picard: disables opening Picard after files download" "-h or --help: prints this help sheet" "-k or --keep: keeps all intermediary files"
         return
     end
     
-    set title (yt-dlp --print '%(title)s' $argv[1])
+    if string match -e "playlist" $argv[1] > /dev/null
+        set comb (yt-dlp --ignore-config --flat-playlist --print playlist_title --print title $argv[1])[2..]
+        set title (string join "/" $comb | string split "/$comb[2]/")
+        set -p title $comb[2]
+        set -e comb
+        mkdir -p $title[1]
+        set folder $PWD/$title[1]
+    else
+        set title (yt-dlp --ignore-config --print '%(title)s' $argv[1])
+        set folder $PWD
+    end
+    
     set tmpdir (mktemp -d -t $title[1])
 
-    builtin echo "downloading $title"
-    set ytfile (yt-dlp -P $tmpdir --exec "builtin echo" -q -f ba -x --audio-format wav $argv[1])
+    builtin echo "downloading $title[1]"
+    set ytfile (yt-dlp --ignore-config --paths $tmpdir --exec "builtin echo" --quiet --format bestaudio --extract-audio --audio-format wav $argv[1])
 
-    builtin echo "converting $title to AAC"
-    # see apple-digital-masters.pdf (soundcheck was too quiet)
-    set filename (string split -m1 -r -f1 "." $ytfile)
-    afconvert $ytfile $filename.caf -d 0 -f caff
-    afconvert $filename.caf -d aac -f m4af -u pgcm 2 -b 256000 -q 127 -s 2 $title.m4a
-    
-    if not set -q _flag_k
-    	$TRASH_EXECUTABLE $tmpdir
-    	builtin echo "deleted intermediary files ($(basename $ytfile) and $(basename $filename.caf))"
+    builtin echo "converting $title[1] to AAC"
+    for i in (seq (count $ytfile))
+        set filename (string split -m1 -r -f1 "." $ytfile[$i])
+        # see apple-digital-masters.pdf (soundcheck was too quiet)
+        afconvert $ytfile[$i] $filename.caf -d 0 -f caff
+        afconvert $filename.caf -d aac -f m4af -u pgcm 2 -b 256000 -q 127 -s 2 $folder/$title[(math $i+1)].m4a
     end
 
-    eval $PICARD_EXECUTABLE \"$PWD/$title.m4a\"
+    if not set -q _flag_k
+    	$TRASH_EXECUTABLE $tmpdir
+    	builtin echo "deleted intermediary files"
+    else
+        builtin echo "intermediary files can be found in $tmpdir"
+    end
+
+    if not set -q _flag_d
+        if test (count $title) -gt 0
+            set parg \"$folder\"
+        else
+            set parg \"$PWD/$title.m4a\"
+        end
+        eval $PICARD_EXECUTABLE $parg
+    end
 end
 
--- functions/ytplwav.fish
+++ functions/ytplwav.fish
@@ -1,3 +1,5 @@
+# this function is redundant since i implemented playlist features into the ytdlwav function but i'll keep it here
+
 function ytplwav
     if not count $argv > /dev/null
         echo 'you forgor the link 💀'
@@ -8,15 +10,18 @@ function ytplwav
         set_color yellow
         echo "warn: argument parsing failed!"
         set_color normal
+        return 1
     end
 
     if set -q _flag_h
-        builtin printf '%s\n' "ytplwav Help" "Parses YT playlist then sends to ytdlwav" "Usage:" "ytdlwav [-h] target_url" "-h or --help: prints this help sheet"
+        builtin printf '%s\n' "ytplwav Help" "Parses YT playlist with jq then sends to ytdlwav" "Usage:" "ytdlwav [-h] target_url" "-h or --help: prints this help sheet"
         return
     end
 
-    builtin echo "downloading and parsing YT playlist information..."
-    set ytids (yt-dlp -j $argv[1] | jq -r ".id")
+    builtin echo "fetching and parsing YT playlist information..."
+    set ytids (yt-dlp -J $argv[1] | jq -r ".title, .entries[].id")
+    builtin echo "downloading $ytids[1]"
+    set -e ytids[1]
 
     for x in $ytids
         builtin echo "downloading YT id $x"
@@ -24,4 +29,5 @@ function ytplwav
     end
 
     builtin echo "finished downloading playlist!"
+    eval $PICARD_EXECUTABLE \"$PWD/$title.m4a\"
 end
