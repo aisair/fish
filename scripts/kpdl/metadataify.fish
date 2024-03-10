645f9da6a759cad085b36fc13873f5f23474752f
aisair <ais11air@gmail.com>
2024-03-09 22:20:32 -0600

    removed legacy kpdl script files

--- scripts/kpdl/metadataify.fish
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env fish
-# this was a separate script that added video metadata before before it was implemented into the kpdl function
-
-mkdir metadatad
-for x in *.m4v
-	set data (string match -r 'Module ([\d\.]+) - (.+) \[' $x)
-	ffmpeg -i $x -c copy -map_metadata 0 -metadata episode_sort=$data[2] -metadata title=$data[3] -metadata season_number=1 -metadata show="MATH 257 Lecture Videos" "./metadatad/$x"
-end
